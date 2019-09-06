606be8ef36fbc3860127015003ac1508e5d6637c
tianxiong <tianxiong@akulaku.com>
2019-09-06 15:21:07 +0800

    基于springboot2.0+实现springcloud的config配置中心.

--- src/main/java/com/tian/ConfigServerGitApplication.java
+++ src/main/java/com/tian/ConfigServerGitApplication.java
@@ -3,16 +3,21 @@ package com.tian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @EnableConfigServer 注解, 用来启用spring-config的配置中心功能.
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerGitApplication {
-
+	/**
+	 * 在git上创建的文件名
+	 * 服务名-环境.yml 或者 服务名-环境.properties
+	 * @param args
+	 */
 	public static void main(String[] args) {
-//		new SpringApplicationBuilder(Application.class).web(true).run(args);
 		SpringApplication.run(ConfigServerGitApplication.class, args);
 	}
 }
